1c7959ab20a8c197dde1b88701d324aad030309c
famulus <famulus.fusion@gmail.com>
2018-03-10 16:31:05 -0500

    channels

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Myapp
   class Application < Rails::Application
         config.api_only = true
         config.assets.enabled = false
+        config.autoload_paths += %W(#{config.root}/app/channels)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
